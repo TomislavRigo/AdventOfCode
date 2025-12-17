7215104689782f32b6afa6d9282dafbfd6092531
Tomislav Rigo <tomislav.rigo@gmail.com>
2025-12-17 22:15:44 +0100

    Day 4 Part 2

--- 2025/day4/part2.go
+++ 2025/day4/part2.go
@@ -1,5 +1,47 @@
 package main
 
+func checkPosition2(coordinate Coordinates, lines []string, xBound int, yBound int) bool {
+	count := 0
+
+	for xoffset := -1; xoffset <= 1; xoffset++ {
+		currentX := coordinate.x + xoffset
+		if currentX == -1 || currentX > xBound {
+			continue
+		}
+
+		for yoffset := -1; yoffset <= 1; yoffset++ {
+			currentY := coordinate.y + yoffset
+			if currentY == -1 || currentY > yBound || (currentX == coordinate.x && currentY == coordinate.y) {
+				continue
+			}
+
+			if lines[currentX][currentY] == '@' {
+				count++
+			}
+		}
+	}
+
+	return count < 4
+}
+
 func part2(lines []string) int {
-	return 0
+	count := 0
+
+	yBound := len(lines) - 1
+	xBound := len(lines[0]) - 1
+
+	for x := 0; x < len(lines); x++ {
+		for y := 0; y < len(lines[0]); y++ {
+			if lines[x][y] == '@' {
+				if checkPosition2(Coordinates{x, y}, lines, xBound, yBound) {
+					count++
+					lines[x] = lines[x][:y] + "." + lines[x][y+1:]
+					x = -1
+					break
+				}
+			}
+		}
+	}
+
+	return count
 }
