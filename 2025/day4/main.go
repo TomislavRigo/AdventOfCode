7d481d24355c9d0e46470848d91d69b8f8f6156e
Tomislav Rigo <tomislav.rigo@gmail.com>
2025-12-17 22:04:12 +0100

    Day 4 Part 1

---
+++ 2025/day4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Coordinates struct {
+	x int
+	y int
+}
+
+func main() {
+	data, err := os.ReadFile("data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+
+	fmt.Println("Part 1: ", part1(lines))
+	fmt.Println("Part 2: ", part2(lines))
+}
---
+++ 2025/day4/part1.go
@@ -0,0 +1,44 @@
+package main
+
+func checkPosition(coordinate Coordinates, lines []string, xBound int, yBound int) bool {
+	count := 0
+
+	for xoffset := -1; xoffset <= 1; xoffset++ {
+		currentX := coordinate.x + xoffset
+		if currentX == -1 || currentX > xBound {
+			continue
+		}
+
+		for yoffset := -1; yoffset <= 1; yoffset++ {
+			currentY := coordinate.y + yoffset
+			if currentY == -1 || currentY > yBound || (currentX == coordinate.x && currentY == coordinate.y) {
+				continue
+			}
+
+			if lines[currentX][currentY] == '@' {
+				count++
+			}
+		}
+	}
+
+	return count < 4
+}
+
+func part1(lines []string) int {
+	count := 0
+
+	yBound := len(lines) - 1
+	xBound := len(lines[0]) - 1
+
+	for x := 0; x < len(lines); x++ {
+		for y := 0; y < len(lines[0]); y++ {
+			if lines[x][y] == '@' {
+				if checkPosition(Coordinates{x, y}, lines, xBound, yBound) {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
