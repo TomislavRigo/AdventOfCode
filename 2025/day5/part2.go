464e9f954894a6182e2c5845a894af3767b8026f
Tomislav Rigo <tomislav.rigo@gmail.com>
2025-12-22 10:34:45 +0100

    - Day 5 Part 2

--- 2025/day5/part2.go
+++ 2025/day5/part2.go
@@ -5,21 +5,54 @@ import (
 	"strings"
 )
 
-func part2(ranges []string) int {
-	total := 0
+func mergeRange(rng Range, mergedRanges []Range) []Range {
+	currentRange := rng
+	for _, mergedRange := range mergedRanges {
+		if currentRange.min >= mergedRange.min && currentRange.max <= mergedRange.max {
+			return mergedRanges
+		}
 
-	validItemRanges := []Range{}
-	for _, r := range ranges {
+		if currentRange.min < mergedRange.min && currentRange.max >= mergedRange.min && currentRange.max <= mergedRange.max {
+			currentRange = Range{currentRange.min, mergedRange.min - 1}
+			continue
+		}
+
+		if currentRange.max > mergedRange.max && currentRange.min >= mergedRange.min && currentRange.min <= mergedRange.max {
+			currentRange = Range{mergedRange.max + 1, currentRange.max}
+			continue
+		}
+
+		if currentRange.min < mergedRange.min && currentRange.max > mergedRange.max {
+			mergedRanges = mergeRange(Range{currentRange.min, mergedRange.min - 1}, mergedRanges)
+			mergedRanges = mergeRange(Range{mergedRange.max + 1, currentRange.max}, mergedRanges)
+
+			return mergedRanges
+		}
+	}
+
+	return append(mergedRanges, currentRange)
+}
+
+func part2(input []string) int {
+	count := 0
+
+	ranges := []Range{}
+	for _, r := range input {
 		parts := strings.Split(r, "-")
 		min, _ := strconv.Atoi(parts[0])
 		max, _ := strconv.Atoi(parts[1])
 
-		validItemRanges = append(validItemRanges, Range{min, max})
+		ranges = append(ranges, Range{min, max})
 	}
 
-	for _, rang := range validItemRanges {
-		total += rang.max - rang.min
+	mergedRanges := []Range{}
+	for _, rng := range ranges {
+		mergedRanges = mergeRange(rng, mergedRanges)
 	}
 
-	return total
+	for _, rng := range mergedRanges {
+		count += rng.max - rng.min + 1
+	}
+
+	return count
 }
